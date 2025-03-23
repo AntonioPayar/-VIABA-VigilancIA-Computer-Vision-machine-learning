a8d21b035821183234acd30522d08b71a7f93505
AntonioPayar <antoniopayar7@gmail.com>
2025-03-23 19:06:29 +0100

    Ruta estatica con la pagina corriendo

--- VigilancIA_FastAPI/src/main.py
+++ VigilancIA_FastAPI/src/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-@app.get("/")
+# Obtiene la ruta absoluta de la carpeta estática
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+
+# Verifica si la carpeta existe
+if not os.path.isdir(static_dir):
+    raise RuntimeError(f"Directory '{static_dir}' does not exist")
+
+# Montar archivos estáticos correctamente
+#http://127.0.0.1:8000/static/configuration_page.html
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+@app.get("/health")
 def read_root():
  return {"greeting":"Hello world"}
