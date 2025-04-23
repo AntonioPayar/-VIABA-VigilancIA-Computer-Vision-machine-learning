afd5e28297b8a2c694cbdc958b42f6690d6ea001
AntonioPayar <63736456+AntonioPayar@users.noreply.github.com>
2025-04-23 22:19:23 +0200

    Feature_KAN-43_Identificacion_rostros_orquestador
    * add : actualizacion de imagen con bounding en monitoring_page
    
    * add : front con bounding en real time
    
    * add : copia seguridad
    
    * add : repositorio ByteTrack modificado
    
    * add : primera implementacion reconocimiento caras offline
    
    * add : reconocimiento personas implementado
    
    * fix : errro merge
    
    * fix : error merge II

--- VigilancIA_FastAPI/src/classes/Clases_Detecciones.py
+++ VigilancIA_FastAPI/src/classes/Clases_Detecciones.py
@@ -1,5 +1,7 @@
 class Persona:
-    def __init__(self, id, confidence, color, lower_right, lower_left, x1, y1, x2, y2):
+    def __init__(
+        self, id, confidence, color, lower_right, lower_left, x1, y1, x2, y2
+    ):
         self.track_id = id
         self.clase = "persona"
         self.confidence = confidence
@@ -10,3 +12,6 @@ class Persona:
         self.y1_imagen_original = y1
         self.x2_imagen_original = x2
         self.y2_imagen_original = y2
+    
+    def setNombre(self, nombre):
+        self.nombre = nombre
--- VigilancIA_FastAPI/src/main.py
+++ VigilancIA_FastAPI/src/main.py
@@ -1,5 +1,4 @@
 # Imports de librerias complejas de python
-# Imports de librerias complejas de python
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -9,12 +8,10 @@ from PIL import Image
 import numpy as np
 
 # Imports de clases propias
-# Imports de clases propias
 from classes.Cordenadas_Configuracion import *
 from monitoreo_offline.utils_offline import *
 from utils_configuration import *
 
-# Imports de librerias sencillas de python
 # Imports de librerias sencillas de python
 import os
 import base64
@@ -34,10 +31,10 @@ STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
 CONFIGURATION = None
 
 
+# Copia de seguridad
 @app.get("/health")
 def read_root():
     return {"greeting": "Hello world"}
-
 
 # Endpoint para la página de configuración
 # http://127.0.0.1:8000/static/configuration_page.html
@@ -48,7 +45,7 @@ app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 @app.post("/setConfiguration")
 async def recibir_datos(datos: DatosCamaras):
     # Guardar los datos en un archivo
-    guardar_datos(datos, "datos_camaras.json")
+    guardar_datos(datos, "files/datos_camaras.json")
     return calcularPixelMapaHomografia(datos.camara1, datos.camara2, 0, 0)
 
 
@@ -60,7 +57,7 @@ async def recibir_datos_pixel_camara01(puntos: CoordenadaXY):
     print(puntos.y)
     # Cargar los datos desde el archivo
     if CONFIGURATION == None:
-        CONFIGURATION = cargar_datos("datos_camaras.json")
+        CONFIGURATION = cargar_datos("files/datos_camaras.json")
     return calcularPixelMapaHomografia(
         CONFIGURATION.camara1, CONFIGURATION.camara2, float(puntos.x), float(puntos.y)
     )
@@ -71,17 +68,20 @@ async def recibir_frame_camara01(data: ImagenBase64):
     global CONFIGURATION
     # Decodificar el base64 y convertir a imagen PIL
     img_pil = Image.open(io.BytesIO(base64.b64decode(data.image_base64)))
+    # Convertir a formato BGR para OpenCV
+    img_bgr = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
+
     np_array = np.array(img_pil.convert("RGB"))
     # Preprocesar la imagen
     np_array = preprocess_image_numpy(add_padding(np_array))
 
     # Realizar la detección de objetos
     if CONFIGURATION == None:
-        CONFIGURATION = cargar_datos("datos_camaras.json")
-        
+        CONFIGURATION = cargar_datos("files/datos_camaras.json")
+
     # Detectar objetos en la imagen
     bounding_boxes, image_bounding = detect_objects(
-        CONFIGURATION.camara1, CONFIGURATION.camara2, np_array
+        CONFIGURATION.camara1, CONFIGURATION.camara2, np_array, img_bgr
     )
     return JSONResponse(
         content={"bounding_boxes": bounding_boxes, "image_bounding": image_bounding}
